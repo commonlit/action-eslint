0e761736c6bafe1d12fb1d5311f4205b88f0f0f3
Geoff Harcourt <geoff.harcourt@gmail.com>
2021-02-07 20:24:58 -0500

    Use Yarn, don't install everything
    Currently to get the right versions of packages, every install builds up
    the entire Yarn bundle for the application, which is very expensive (it
    takes about half of our total processing time). This approach opts to
    have the action caller specify the packages they want (including
    potentially versions) to get things set and reduce prep time.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,11 @@ ESLINT_FORMATTER='/formatter.js'
 
 cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
-if [ ! -f "$(npm bin)/eslint" ]; then
-  npm install --legacy-peer-deps --ignore-scripts --force
-fi
+yarn global add $INPUT_ESLINT_PACKAGES
 
-$(npm bin)/eslint --version
+$(yarn global bin)/eslint --version
 
-$(npm bin)/eslint -f="${ESLINT_FORMATTER}" ${INPUT_ESLINT_FLAGS:-'.'} \
+$(yarn global bin)/eslint -f="${ESLINT_FORMATTER}" ${INPUT_ESLINT_FLAGS:-'.'} \
   | reviewdog -f=rdjson \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-review}" \
