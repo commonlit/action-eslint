28616c7944c7ecca58efeb6819b19b9efdc1574b
Geoff Harcourt <geoff.harcourt@gmail.com>
2021-02-08 16:55:02 -0500

    Use jq to filter package.json down
    Currently the upstream action installs every package in our bundle to
    run ESLint. This adds minutes to each linter run. We started to work
    around this by only installing the packages we wanted, but it meant the
    linter would install the most recent versions of packages vs. what we've
    been using for local development (the versions in our `yarn.lock` file).
    
    We can work around this and retain version locking by stripping out the
    dependencies we don't want. This change uses `jq` to edit our
    package.json file inside the action. It works like this:
    
    1. iterate over the sections. If the section isn't `dependencies` or
       `devDependencies`, keep it as-is.
    2. if the section contains dependencies, iterate over the keys of the
       section. Filter out any elements that don't contain the string
       `eslint` or is the `jest` or `typescript` package. Every package we
       use contains `eslint` in its name, but we can change this in the
       future if that's not true.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,15 @@ ESLINT_FORMATTER='/formatter.js'
 
 cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
-yarn global add $INPUT_ESLINT_PACKAGES
+# Rewrite package.json so that it only contains the packages we're about to use.
+# The lockfile will remain the same so the package versions should match what
+# we have checked in as the versions for local development.
+echo $(cat package.json | jq 'to_entries | map(if .key == "dependencies" or .key == "devDependencies" then . + {"value": .value | to_entries | map(select(.key | test("(eslint|\\A(jest|typescript)\\Z)"))) | from_entries} else . end) | from_entries') > package.json
+yarn install
 
-$(yarn global bin)/eslint --version
+$(yarn bin)/eslint --version
 
-$(yarn global bin)/eslint -f="${ESLINT_FORMATTER}" ${INPUT_ESLINT_FLAGS:-'.'} \
+$(yarn bin)/eslint -f="${ESLINT_FORMATTER}" ${INPUT_ESLINT_FLAGS:-'.'} \
   | reviewdog -f=rdjson \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-review}" \
